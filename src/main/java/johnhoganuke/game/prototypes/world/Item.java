0393c79a80c92f684b76a35f2b6d860f689d16aa
John Kayne <johnhoganuke2@gmail.com>
2016-05-12 13:02:44 -0400

    Update and rename src/main/java/johnhoganuke/game/world/Item.java to src/main/java/johnhoganuke/game/prototypes/world/Item.java

---
+++ src/main/java/johnhoganuke/game/prototypes/world/Item.java
@@ -0,0 +1,10 @@
+package johnhoganuke.game.prototypes.world;
+
+import johnhoganuke.game.prototypes.world;
+import johnhoganuke.jogen.prototypes.RenderClass;
+
+public class Item {
+  String name;
+  SubstanceInt [] madeOf;
+  RenderClass render;
+}
