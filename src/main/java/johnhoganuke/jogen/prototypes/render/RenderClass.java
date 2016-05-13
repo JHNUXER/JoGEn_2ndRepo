ea324c7ee7b67b0eac1fcacac327cd40ae5435cf
John Kayne <johnhoganuke2@gmail.com>
2016-05-13 10:54:03 -0400

    Update RenderClass.java

--- src/main/java/johnhoganuke/jogen/prototypes/render/RenderClass.java
+++ src/main/java/johnhoganuke/jogen/prototypes/render/RenderClass.java
@@ -1,1 +1,9 @@
-public class RenderClass {}
+package johnhoganuke.jogen.prototypes.render;
+
+import johnhoganuke.jogen.prototypes.render.Model;
+import johnhoganuke.jogen.prototypes.render.Texture;
+
+public class RenderClass {
+  Model objectModel;
+  Texture objectTexture;
+}
