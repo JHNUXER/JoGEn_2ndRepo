4fd2059e696dbdc97e2e5dd7b7256c9d5de547fb
John Kayne <johnhoganuke2@gmail.com>
2016-05-13 10:56:46 -0400

    Update Model.java

--- src/main/java/johnhoganuke/jogen/prototypes/render/Model.java
+++ src/main/java/johnhoganuke/jogen/prototypes/render/Model.java
@@ -1,3 +1,11 @@
+package johnhoganuke.jogen.prototypes.render;
+
+import johnhoganuke.jogen.prototypes.render.Pos;
+import johnhoganuke.jogen.prototypes.render.Line;
+import johnhoganuke.jogen.prototypes.render.Plane;
+
 public class Model {
-  
+  Pos [] vertices;
+  Line [] edges;
+  Plane [] faces;
 }
