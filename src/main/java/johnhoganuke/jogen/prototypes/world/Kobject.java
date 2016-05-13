c944c71c1957e63616e51ae2c71c00329c32fc0a
John Kayne <johnhoganuke2@gmail.com>
2016-05-13 10:50:22 -0400

    Update Kobject.java

--- src/main/java/johnhoganuke/jogen/prototypes/world/Kobject.java
+++ src/main/java/johnhoganuke/jogen/prototypes/world/Kobject.java
@@ -1,4 +1,11 @@
+package johnhoganuke.jogen.prototypes.world;
+
+import johnhoganuke.jogen.prototypes.Pos;
+import johnhoganuke.jogen.prototypes.render.RenderClass;
+import johnhoganuke.jogen.prototypes.world.Substance;
+
 public class Kobject {
   String name;
-  Pos position;
+  RenderClass render;
+  Substance [] madeOf;
 }
