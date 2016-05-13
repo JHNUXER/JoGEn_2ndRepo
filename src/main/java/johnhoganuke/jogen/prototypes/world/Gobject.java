6a74a9b9aebd9a40e6ed8650c58fb3b85713d6bf
John Kayne <johnhoganuke2@gmail.com>
2016-05-13 10:51:57 -0400

    Update Gobject.java

--- src/main/java/johnhoganuke/jogen/prototypes/world/Gobject.java
+++ src/main/java/johnhoganuke/jogen/prototypes/world/Gobject.java
@@ -1,1 +1,8 @@
-public class Gobject {}
+package johnhoganuke.jogen.prototypes.world;
+
+import johnhoganuke.jogen.prototypes.Pos;
+import johnhoganuke.jogen.prototypes.world.Kobject;
+
+public class Gobject extends Kobject {
+  Pos position;
+}
