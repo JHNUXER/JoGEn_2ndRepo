18776da17c4f4d1d4bc71359f22e1c5abf0b9b2c
John Kayne <johnhoganuke2@gmail.com>
2016-05-13 10:34:11 -0400

    INCOMPLETE

--- src/main/java/johnhoganuke/game/prototypes/world/machine/Machine_SmallReactor.java
+++ src/main/java/johnhoganuke/game/prototypes/world/machine/Machine_SmallReactor.java
@@ -1,1 +1,5 @@
-public class Machine_SmallReactor {}
+public class Machine_SmallReactor extends Machine, EnergyProducer {
+  name = "Small Nuclear Reactor";
+  madeOf = {{Substance_Steel, 100}};
+  render = E_Render.
+}
