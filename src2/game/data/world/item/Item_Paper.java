dc99d4f7d2fcc80aa45cb39319b2fe0a520d5648
John Kayne <johnhoganuke2@gmail.com>
2016-05-23 10:00:53 -0400

    Create Item_Paper.java

---
+++ src2/game/data/world/item/Item_Paper.java
@@ -0,0 +1,9 @@
+package game.data.world.item;
+
+import game.data.AllSubstances;
+
+public class Item_Paper {
+  name = "Paper";
+  madeOf = {{Substance_Paper}};
+  rend = game.data.render.item.paper;
+}
